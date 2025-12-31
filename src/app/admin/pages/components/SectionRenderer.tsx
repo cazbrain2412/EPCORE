d8ce763e79c4a12ebe37c102c24d24a30dbe6ddc
cazbrain2412 <cazbraingroup@gmail.com>
2025-12-31 14:42:30 +0530

    Fix TS build: type SectionRenderer props

--- src/app/admin/pages/components/SectionRenderer.tsx
+++ src/app/admin/pages/components/SectionRenderer.tsx
@@ -2,7 +2,8 @@ import HeroLeadForm from "./sections/HeroLeadForm";
 import SplitMedia from "./sections/SplitMedia";
 import FAQ from "./sections/FAQ";
 
-export default function SectionRenderer({ section }) {
+export default function SectionRenderer({ section }: { section: any }) {
+
   switch (section.type) {
     case "hero_lead_form":
       return <HeroLeadForm {...section} />;
